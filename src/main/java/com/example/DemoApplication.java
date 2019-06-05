5606309c2688078eff6768cea6f45e503f1ac5dd
Clarence D'Silva <clarence.dsilva@hotmail.com>
2019-06-04 23:51:31 -0400

    Modified the main method

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,9 @@
 package com.example;
 
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.util.JSON_Reader;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,20 +11,6 @@ public class DemoApplication {
 		JSON_Reader.loadSchema();
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("HELLO CSYE7255!!\n");
-		String inputJson = JSON_Reader.readJSONFile();
-		//System.out.println("input json: " + inputJson);
-
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		JsonNode rootNode;
-//		try {
-//			rootNode = objectMapper.readTree(inputJson);
-//			System.out.printf("root: %s type=%s%n", rootNode, rootNode.getNodeType());
-//			JSON_Reader.addKeys(inputJson, rootNode, map, suffix);
-//			//JSON_Reader.traverse(rootNode, 1);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
 	}
 
 }
