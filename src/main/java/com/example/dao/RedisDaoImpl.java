ef942ecf5ec28426d62022619a612ad0a054e62d
Clarence D'Silva <clarence.dsilva@hotmail.com>
2019-07-02 22:33:41 -0400

    Implemented PUT and PATCH methods, Secured REST API using OAuth2.0 and added Etag filters to application

--- src/main/java/com/example/dao/RedisDaoImpl.java
+++ src/main/java/com/example/dao/RedisDaoImpl.java
@@ -1,9 +1,13 @@
 package com.example.dao;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties.Jedis;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -47,7 +51,7 @@ public class RedisDaoImpl<T> implements RedisDao<T> {
 	public void putValueWithExpireTime(String key, Object value, long timeout, TimeUnit unit) {
 		valueOperations.set(key, (T) value, timeout, unit);
 	}
-
+	@Override
 	public T getValue(String key) {
 		return valueOperations.get(key);
 	}
@@ -59,6 +63,19 @@ public class RedisDaoImpl<T> implements RedisDao<T> {
 	@Override
 	public boolean deleteValue(String key) {
 		return redisTemplate.delete(key);
+	}
+	
+	@Override
+	public long deleteAll() {
+		//redisTemplate.opsForList().rightPushIfPresent(key, value);
+		String keys = "*";
+		Collection<String> allKeys = new HashSet<String>();
+		allKeys = redisTemplate.keys(keys);
+		return redisTemplate.delete(allKeys);
+	}
+	
+	public T getHash(String input) {
+		return valueOperations.get(input + "_hash");
 	}
 
 }
